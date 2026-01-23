0880287b817d7a267377328333f162c3533ccabd
mirra_dragnil <mirradragnil@gmail.com>
2026-01-23 14:21:48 +0300

    test

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Тестовый заголовок</title>
+</head>
+<body>
+Просто проверка работы github.io
+</body>
+</html>
